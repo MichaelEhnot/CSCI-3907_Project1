30edeb9afaafc8d0f176045f32f871f9207325c4
MichaelEhnot <mehnot@gwu.edu>
2022-01-21 22:17:00 -0500

    multi scale done

--- main.py
+++ main.py
@@ -7,14 +7,43 @@ import numpy as np
 import skimage as sk
 import skimage.io as skio
 
+from skimage.transform import rescale
 from matplotlib import pyplot as plt
 
-# find the best allignment of two channels, Single Scale
-def align_ss(u, v):
+def align_ms(u, v):
+    tempU = rescale(u, .25)
+    tempV = rescale(v, .25)
+
+    al, x1, y1 = align_ss(tempU,tempV, 20)
+    print("25%")
+
+    tempU = rescale(u, .5)
+    tempV = rescale(v, .5)
+    tempU = np.roll(tempU, (x1*2), axis=0)
+    tempU = np.roll(tempU, (y1*2), axis=1)
+
+    al, x2, y2 = align_ss(tempU,tempV, 10)
+    print("50%")
+
+    tempU = u
+    tempV = v
+    tempU = np.roll(tempU, ((x2*2)+(x1*4)), axis=0)
+    tempU = np.roll(tempU, ((y2*2)+(y1*4)), axis=1)
+
+    al, x, y = align_ss(tempU,tempV, 5)
+    print("100%")
+
+    return al
+
+# find the best allignment of two channels, Single Scale, rng is the range of pixels the channels will slide in both directions
+# range should be positive
+def align_ss(u, v, rng):
     # start with max value
     lowScore = 999999999
-    for x in range(-15, 16):
-        for y in range(-15, 16):
+
+    #check a 30 pixel range in x and y direction
+    for x in range(-1*(rng), (rng+1)):
+        for y in range(-1*(rng), (rng+1)):
             temp = np.roll(u, x, axis=0)
             temp = np.roll(temp, y, axis=1)
             score = np.sum( (temp-v)**2)
@@ -26,10 +55,11 @@ def align_ss(u, v):
     print('Best Y: '+ str(bestY))
     temp = np.roll(u, bestX, axis=0)
     temp = np.roll(temp, bestY, axis=1)
-    return temp
+    return temp, bestX, bestY
 
 # name of the input file
-imname = './data/31421v.jpg'
+#imname = './data/01047u.jpg'
+imname = input("Enter image path: ")
 
 # read in the image
 im = skio.imread(imname)
@@ -49,8 +79,13 @@ r = im[2*height: 3*height]
 # functions that might be useful for aligning the images include:
 # np.roll, np.sum, sk.transform.rescale (for multiscale)
 
-ag = align_ss(g, b)
-ar = align_ss(r, b)
+# single scale
+#ag, x, y = align_ss(g, b, 15)
+#ar, x, y = align_ss(r, b, 15)
+
+# multi scale
+ag = align_ms(g, b)
+ar = align_ms(r, b)
 
 # create a color image
 im_out = np.dstack([ar, ag, b])
